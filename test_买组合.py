12db534dc797fc2170fba4fdfea67ae85314ee83
wangkangrui <649442561@qq.com>
2023-04-18 13:18:20 +0800

    version 1.0.2 添加买组合，修改买基金到组合（增加免密接口）

--- test_买组合.py
+++ test_买组合.py
@@ -9,8 +9,8 @@ import allure
 from conftest import read_yaml2, write_yaml2, write_yaml3, read_yaml3, clear_yaml3, read_yaml1, read_yaml4
 
 
-@allure.feature('买基金到组合 660以下输入密码')
-class Test_buy_fund_to_Sub():
+@allure.feature('买组合 660以下输入密码')
+class Test_buy_Sub():
     @allure.story('交易留痕 /Business/home/NoticeStayTrace')
     # 交易留痕
     def test_Business_home_NoticeStayTrace(self):
@@ -25,28 +25,30 @@ class Test_buy_fund_to_Sub():
         TraceID = res.json()["Data"]["TraceID"]
         write_yaml3({"TraceID": TraceID})
 
-    @allure.story('买基金到组合 /Trade/FundTrade/CommitOrder')
-    # 买基金到组合 660以下输密码
-    def test_Trade_FundTrade_CommitOrder(self):
-        url = urljoin(read_yaml1()[read_yaml4()["Env"]], "/Trade/FundTrade/CommitOrder")
+    @allure.story('买组合 /User/SubA/CommitOrderCus')
+    # 买组合 660以下输密码
+    def test_User_SubA_CommitOrderCus(self):
+        url = urljoin(read_yaml1()[read_yaml4()["Env"]], "/User/SubA/CommitOrderCus")
         datas = {
             "Password": read_yaml1()[read_yaml4()["Pas"]],
-            "TradeType": read_yaml4()["TradeType"],
+            "TradeType": read_yaml4()["TradeType_Sub"],
             "UserId": read_yaml2()["CustomerNo"],
             "BankAccountNo": read_yaml2()["BankAccountNo"],
             "CouponsType": "",
             "CouponsId": "",
-            "FundCode": "000001",
-            "TotalAmounts": 10.00,
-            "FundAppsJson": read_yaml4()["FundAppsJson"],
+            "FundCode": "",
+            "TotalAmounts": read_yaml4()["Amount"],
+            "FundAppsJson": "",
             "TraceID": read_yaml3()["TraceID"],
             "RecommanderNo": "",
-            "SubAccountNo": read_yaml2()["SubAccountNo"],
-            "FollowingSubAccountNo": "",
+            "SubAccountNo": "",
+            "FollowingSubAccountNo": read_yaml2()["SubAccountNo"],
             "IsRemittance": "false",
             "IsPayPlus": "false",
             "RatioRefundType": "",
+            "TradeFlow": "default",
             "InstalledApp": "false",
+            "AppScheme": "",
 
             "PhoneType": "IPhone",
             "ServerVersion": "6.5.8",
@@ -54,36 +56,37 @@ class Test_buy_fund_to_Sub():
             "UToken": read_yaml2()["UToken"],
             "MobileKey": "01F12605-0E93-4BCB-AD67-D46C1DDA604B"
         }
+
         res = requests.request(method='post', url=url, params=datas)
         ErrorCode = res.json()["ErrorCode"]
-        AppSerialNo = res.json()["Data"]["AppSerialNo"]
-        BusinType = res.json()["Data"]["BusinType"]
+        ContextId = res.json()["Data"]["ContextId"]
 
         clear_yaml3()
-        write_yaml3({"AppSerialNo": AppSerialNo})
-        write_yaml3({"BusinType": BusinType})
+        write_yaml3({"ContextId": ContextId})
+
         with allure.step('接口是否正常调通'):
             if res.status_code == 200:
                 assert True
             else:
                 assert False, '接口状态码非200'
-        with allure.step('买基金到组合请求成功'):
+        with allure.step('买组合请求成功'):
             if ErrorCode == 0:
-                assert True
+                assert True, '买组合请求成功'
             else:
                 assert False, '登录状态已过期，请重新登录'
         time.sleep(5)
 
-    @allure.story('买基金到组合结果页 /Trade/FundTrade/OrderResult')
-    def test_Trade_FundTrade_OrderResult(self):
-        url = urljoin(read_yaml1()[read_yaml4()["Env"]], "/Trade/FundTrade/OrderResult")
+    @allure.story('买组合结果页 /User/SubA/CompleteOrderCus')
+    def test_User_SubA_CompleteOrderCus(self):
+        url = urljoin(read_yaml1()[read_yaml4()["Env"]], "/User/SubA/CompleteOrderCus")
         datas = {
-            "AppSerialNo": read_yaml3()["AppSerialNo"],
-            "ParentAppSerialNo": '',
-            "BusinType": read_yaml3()["BusinType"],
-            "Mode": '',
-            "TradeModeType": '',
             "UserId": read_yaml2()["CustomerNo"],
+            "ContextId": read_yaml3()["ContextId"],
+            "VerifyCode": "",
+            "BankAppPayResult": "",
+            "BankCallBackContextId": "",
+            "TradeFlow": "",
+            "CancelPay": "false",
 
             "PhoneType": "IPhone",
             "ServerVersion": "6.5.8",
@@ -91,6 +94,7 @@ class Test_buy_fund_to_Sub():
             "UToken": read_yaml2()["UToken"],
             "MobileKey": "01F12605-0E93-4BCB-AD67-D46C1DDA604B"
         }
+
         res = requests.request(method='post', url=url, params=datas)
         ErrorCode = res.json()["ErrorCode"]
         PayError = res.json()["Data"]["PayError"]
@@ -102,14 +106,14 @@ class Test_buy_fund_to_Sub():
                 assert True
             else:
                 assert False, '接口状态码非200'
-        with allure.step('买基金到组合受理结果是否正常展示'):
+        with allure.step('买组合受理结果是否正常展示'):
             if ErrorCode == 0:
                 assert True
             else:
                 assert False, '登录状态已过期，请重新登录'
-        with allure.step('买基金到组合受理结果是否成功'):
+        with allure.step('买组合受理结果是否成功'):
             if PayError == '':
-                assert True, '买基金到组合成功'
+                assert True, '买组合成功'
             else:
                 if PayError == '1':
                     assert False, '错误原因：账户余额不足'
@@ -120,8 +124,8 @@ class Test_buy_fund_to_Sub():
                         assert False
 
 
-@allure.feature('买基金到组合 660以下免密')
-class Test_buy_fund_to_Sub_NP():
+@allure.feature('买组合 660以下免密')
+class Test_buy_Sub_NP():
     @allure.story('交易留痕 /Business/home/NoticeStayTrace')
     # 交易留痕
     def test_Business_home_NoticeStayTrace(self):
@@ -136,27 +140,29 @@ class Test_buy_fund_to_Sub_NP():
         TraceID = res.json()["Data"]["TraceID"]
         write_yaml3({"TraceID": TraceID})
 
-    @allure.story('买基金到组合免密 /Trade/FundTrade/CommitOrderNP')
-    # 买基金到组合 660以下免密
-    def test_Trade_FundTrade_CommitOrder(self):
-        url = urljoin(read_yaml1()[read_yaml4()["Env"]], "/Trade/FundTrade/CommitOrderNP")
+    @allure.story('买组合 /User/SubA/CommitOrderCusNP')
+    # 买组合 660以下免密
+    def test_User_SubA_CommitOrderCusNP(self):
+        url = urljoin(read_yaml1()[read_yaml4()["Env"]], "/User/SubA/CommitOrderCusNP")
         datas = {
-            "TradeType": read_yaml4()["TradeType"],
+            "TradeType": read_yaml4()["TradeType_Sub"],
             "UserId": read_yaml2()["CustomerNo"],
             "BankAccountNo": read_yaml2()["BankAccountNo"],
             "CouponsType": "",
             "CouponsId": "",
-            "FundCode": "000001",
-            "TotalAmounts": 10.00,
-            "FundAppsJson": read_yaml4()["FundAppsJson"],
+            "FundCode": "",
+            "TotalAmounts": read_yaml4()["Amount"],
+            "FundAppsJson": "",
             "TraceID": read_yaml3()["TraceID"],
             "RecommanderNo": "",
-            "SubAccountNo": read_yaml2()["SubAccountNo"],
-            "FollowingSubAccountNo": "",
+            "SubAccountNo": "",
+            "FollowingSubAccountNo": read_yaml2()["SubAccountNo"],
             "IsRemittance": "false",
             "IsPayPlus": "false",
             "RatioRefundType": "",
+            "TradeFlow": "default",
             "InstalledApp": "false",
+            "AppScheme": "",
 
             "PhoneType": "IPhone",
             "ServerVersion": "6.5.8",
@@ -164,36 +170,37 @@ class Test_buy_fund_to_Sub_NP():
             "UToken": read_yaml2()["UToken"],
             "MobileKey": "01F12605-0E93-4BCB-AD67-D46C1DDA604B"
         }
+
         res = requests.request(method='post', url=url, params=datas)
         ErrorCode = res.json()["ErrorCode"]
-        AppSerialNo = res.json()["Data"]["AppSerialNo"]
-        BusinType = res.json()["Data"]["BusinType"]
+        ContextId = res.json()["Data"]["ContextId"]
 
         clear_yaml3()
-        write_yaml3({"AppSerialNo": AppSerialNo})
-        write_yaml3({"BusinType": BusinType})
+        write_yaml3({"ContextId": ContextId})
+
         with allure.step('接口是否正常调通'):
             if res.status_code == 200:
                 assert True
             else:
                 assert False, '接口状态码非200'
-        with allure.step('买基金到组合请求成功'):
+        with allure.step('买组合请求成功'):
             if ErrorCode == 0:
-                assert True
+                assert True, '买组合请求成功'
             else:
                 assert False, '登录状态已过期，请重新登录'
         time.sleep(5)
 
-    @allure.story('买基金到组合结果页 /Trade/FundTrade/OrderResult')
-    def test_Trade_FundTrade_OrderResult(self):
-        url = urljoin(read_yaml1()[read_yaml4()["Env"]], "/Trade/FundTrade/OrderResult")
+    @allure.story('买组合结果页 /User/SubA/CompleteOrderCus')
+    def test_User_SubA_CompleteOrderCus(self):
+        url = urljoin(read_yaml1()[read_yaml4()["Env"]], "/User/SubA/CompleteOrderCus")
         datas = {
-            "AppSerialNo": read_yaml3()["AppSerialNo"],
-            "ParentAppSerialNo": '',
-            "BusinType": read_yaml3()["BusinType"],
-            "Mode": '',
-            "TradeModeType": '',
             "UserId": read_yaml2()["CustomerNo"],
+            "ContextId": read_yaml3()["ContextId"],
+            "VerifyCode": "",
+            "BankAppPayResult": "",
+            "BankCallBackContextId": "",
+            "TradeFlow": "",
+            "CancelPay": "false",
 
             "PhoneType": "IPhone",
             "ServerVersion": "6.5.8",
@@ -201,6 +208,7 @@ class Test_buy_fund_to_Sub_NP():
             "UToken": read_yaml2()["UToken"],
             "MobileKey": "01F12605-0E93-4BCB-AD67-D46C1DDA604B"
         }
+
         res = requests.request(method='post', url=url, params=datas)
         ErrorCode = res.json()["ErrorCode"]
         PayError = res.json()["Data"]["PayError"]
@@ -212,14 +220,14 @@ class Test_buy_fund_to_Sub_NP():
                 assert True
             else:
                 assert False, '接口状态码非200'
-        with allure.step('买基金到组合受理结果是否正常展示'):
+        with allure.step('买组合受理结果是否正常展示'):
             if ErrorCode == 0:
                 assert True
             else:
                 assert False, '登录状态已过期，请重新登录'
-        with allure.step('买基金到组合受理结果是否成功'):
+        with allure.step('买组合受理结果是否成功'):
             if PayError == '':
-                assert True, '买基金到组合成功'
+                assert True, '买组合成功'
             else:
                 if PayError == '1':
                     assert False, '错误原因：账户余额不足'
