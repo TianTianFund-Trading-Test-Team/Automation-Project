88e5a2fc687f7dcd0613682d8f03ecb3aca38590
wangkangrui <649442561@qq.com>
2023-04-21 15:14:23 +0800

    version 1.0.8 添加撤单接口

--- test_组合内QD基金转换.py
+++ test_组合内QD基金转换.py
@@ -9,7 +9,7 @@ import allure
 from conftest import read_yaml2, write_yaml2, write_yaml3, read_yaml3, clear_yaml3, read_yaml1, read_yaml4
 
 
-@allure.feature('组合内QD基金普通卖出 890')
+@allure.feature('组合内QD基金 890')
 class Test_redeem_QDFund_Sub():
     @allure.story('子账户持仓 /User/Asset/GetFundAssetListOfSubV2')
     # 获取子账户持仓 QD型
@@ -152,10 +152,137 @@ class Test_redeem_QDFund_Sub():
             with allure.step('组合内QD基金转换是否成功'):
                 if ErrorCode == 0:
                     assert True, '申请受理成功'
+                    clear_yaml3()
+                    BusinSerialNo = res.json()["Data"]["JumpParams"]["BusinSerialNo"]
+                    BusinessType = res.json()["Data"]["JumpParams"]["BusinessType"]
+                    write_yaml3({"BusinSerialNo": BusinSerialNo})
+                    write_yaml3({"BusinessType": BusinessType})
                 else:
                     assert False, ErrorMessage
 
-    @allure.story('交易留痕2 890免密 /Business/home/NoticeStayTrace')
+    @allure.story('890撤单 /Trade/FundTrade/RevokeOrder')
+    # 890撤单
+    def test_Trade_FundTrade_RevokeOrder(self):
+        if read_yaml3() is None:
+            pytest.skip(), '没有QD基金份额或者份额不足无法发起超级转换'
+        else:
+            url = urljoin(read_yaml1()[read_yaml4()["Env"]], "/Trade/FundTrade/RevokeOrder")
+            datas = {
+                "Password": read_yaml1()[read_yaml4()["Pas"]],
+                "UserId": read_yaml2()["CustomerNo"],
+                "BusinId": read_yaml3()["BusinSerialNo"],
+                "BusinType": read_yaml3()["BusinessType"],
+                "IsRevokedToCashBag": "false",
+                "DisplayBusinType": read_yaml3()["BusinessType"],
+
+                "PhoneType": "IPhone",
+                "ServerVersion": "6.5.8",
+                "CToken": read_yaml2()["CToken"],
+                "UToken": read_yaml2()["UToken"],
+                "MobileKey": "01F12605-0E93-4BCB-AD67-D46C1DDA604B"
+            }
+            res = requests.request(method='post', url=url, params=datas)
+            with allure.step('接口是否正常调通'):
+                if res.status_code == 200:
+                    assert True
+                else:
+                    assert False, '接口状态码非200'
+            ErrorCode = res.json()["ErrorCode"]
+            ErrorMessage = res.json()["ErrorMessage"]
+            with allure.step('QD基金撤单是否成功'):
+                if ErrorCode == 0:
+                    assert True, '撤单受理成功'
+                else:
+                    assert False, ErrorMessage
+
+
+@allure.feature('组合内QD基金 免密 890')
+class Test_redeem_QDFund_Sub_NP():
+    @allure.story('子账户持仓 /User/Asset/GetFundAssetListOfSubV2')
+    # 获取子账户持仓 QD型
+    def test_User_Asset_GetFundAssetListOfSubV2(self):
+        clear_yaml3()
+        url = urljoin(read_yaml1()[read_yaml4()["Env"]], "/User/Asset/GetFundAssetListOfSubV2")
+        datas = {
+            "UserId": read_yaml2()["CustomerNo"],
+            "SubAccountNo": read_yaml2()["SubAccountNo"],
+            "UnifiedType": "QD",  # 筛选类型： HH 混合； ZS 指数； GP 股票； ZQ 债券； QD QD基金； HB 货币基金
+            "BankCardNo": "",
+
+            "PhoneType": "IPhone",
+            "ServerVersion": "6.5.8",
+            "CToken": read_yaml2()["CToken"],
+            "UToken": read_yaml2()["UToken"],
+            "MobileKey": "01F12605-0E93-4BCB-AD67-D46C1DDA604B"
+        }
+        res = requests.request(method='post', url=url, params=datas)
+        with allure.step('接口是否正常调通'):
+            if res.status_code == 200:
+                assert True
+            else:
+                assert False, '接口状态码非200'
+        Res = res.json()["Data"]["AssetDetails"]
+        with allure.step('组合里是否有QD基金'):
+            if Res == []:
+                clear_yaml3()
+                assert False, '组合内没有QD基金'
+            else:
+                assert True
+                AssetValue = res.json()["Data"]["AssetDetails"][0]["AssetValue"]
+                with allure.step('QD基金是否有足够份额'):
+                    if AssetValue > '15':
+                        assert True
+                        FundCode = res.json()["Data"]["AssetDetails"][0]["FundCode"]
+                        clear_yaml3()
+                        write_yaml3({"FundCode": FundCode})
+                    else:
+                        clear_yaml3()
+                        assert False, '份额不足无法发起'
+
+    @allure.story('持仓详情（组合） /User/home/GetShareDetail')
+    # 获取特定基金的组合份额
+    def test_User_home_GetShareDetail(self):
+        if read_yaml3() is None:
+            pytest.skip(), '没有QD基金份额或者份额不足无法发起890'
+        else:
+            url = urljoin(read_yaml1()[read_yaml4()["Env"]], "/User/home/GetShareDetail")
+            datas = {
+                "UserId": read_yaml2()["CustomerNo"],
+                "SubAccountNo": read_yaml2()["SubAccountNo"],
+                "FundCode": read_yaml3()["FundCode"],
+                "IsBaseAsset": "false",
+                "TransactionAccountId": "",
+                "NeedReturnZeroVolItems": "false",
+
+                "PhoneType": "IPhone",
+                "ServerVersion": "6.5.8",
+                "CToken": read_yaml2()["CToken"],
+                "UToken": read_yaml2()["UToken"],
+                "MobileKey": "01F12605-0E93-4BCB-AD67-D46C1DDA604B"
+            }
+            res = requests.request(method='post', url=url, params=datas)
+            with allure.step('接口是否正常调通'):
+                if res.status_code == 200:
+                    assert True
+                else:
+                    assert False, '接口状态码非200'
+            with allure.step('是否存在份额可以QD转换'):  # 因为多卡才做的逻辑。这个接口没有排序也没有字段返回份额数量
+                for i in range(5):
+                    try:
+                        if res.json()["Data"]["Shares"][i - 1]["AvailableShare"] > 14:
+                            AvailableShare = res.json()["Data"]["Shares"][i - 1]["AvailableShare"]
+                            ShareId = res.json()["Data"]["Shares"][i - 1]["ShareId"]
+                            write_yaml3({"ShareId": ShareId})
+                            write_yaml3({"AvailableShare": AvailableShare})
+                            break
+                    except IndexError:
+                        pass
+
+                if read_yaml3() == {'FundCode': read_yaml3()["FundCode"]}:
+                    clear_yaml3()
+                    assert False, '没有足够的QD基金份额做890'
+
+    @allure.story('交易留痕 890免密 /Business/home/NoticeStayTrace')
     # 交易留痕2
     def test_Business_home_NoticeStayTrace2(self):
         if read_yaml3() is None:
@@ -169,8 +296,8 @@ class Test_redeem_QDFund_Sub():
                 "UToken": read_yaml2()["UToken"]
             }
             res = requests.request(method='post', url=url, params=datas)
-            TraceID2 = res.json()["Data"]["TraceID"]
-            write_yaml3({"TraceID2": TraceID2})
+            TraceID = res.json()["Data"]["TraceID"]
+            write_yaml3({"TraceID": TraceID})
 
     @allure.story('组合内QD基金转换 免密 /Business/Home/SFT1TransferNP')
     # 组合内QD基金转换 免密
@@ -191,7 +318,7 @@ class Test_redeem_QDFund_Sub():
                 "FundOut": read_yaml3()["FundCode"],
                 "FundAmount": read_yaml4()["FundAmount_CJZH"],
                 "LargeRedemptionFlag": "1",
-                "TraceID": read_yaml3()["TraceID2"],  # 890放一个类，TraceID会有两个
+                "TraceID": read_yaml3()["TraceID"],  # 890放一个类，TraceID会有两个
                 "FromSubAccountNo": read_yaml2()["SubAccountNo"],
                 "ToSubAccountNo": read_yaml2()["SubAccountNo"],
 
@@ -212,5 +339,44 @@ class Test_redeem_QDFund_Sub():
             with allure.step('组合内QD基金转换是否成功'):
                 if ErrorCode == 0:
                     assert True, '申请受理成功'
+                    clear_yaml3()
+                    BusinSerialNo = res.json()["Data"]["JumpParams"]["BusinSerialNo"]
+                    BusinessType = res.json()["Data"]["JumpParams"]["BusinessType"]
+                    write_yaml3({"BusinSerialNo": BusinSerialNo})
+                    write_yaml3({"BusinessType": BusinessType})
+                else:
+                    assert False, ErrorMessage
+
+    @allure.story('890撤单 免密 /Trade/FundTrade/RevokeOrderNP')
+    # 890撤单 免密
+    def test_Trade_FundTrade_RevokeOrderNP(self):
+        if read_yaml3() is None:
+            pytest.skip(), '没有QD基金份额或者份额不足无法发起超级转换'
+        else:
+            url = urljoin(read_yaml1()[read_yaml4()["Env"]], "/Trade/FundTrade/RevokeOrderNP")
+            datas = {
+                "UserId": read_yaml2()["CustomerNo"],
+                "BusinId": read_yaml3()["BusinSerialNo"],
+                "BusinType": read_yaml3()["BusinessType"],
+                "IsRevokedToCashBag": "false",
+                "DisplayBusinType": read_yaml3()["BusinessType"],
+
+                "PhoneType": "IPhone",
+                "ServerVersion": "6.5.8",
+                "CToken": read_yaml2()["CToken"],
+                "UToken": read_yaml2()["UToken"],
+                "MobileKey": "01F12605-0E93-4BCB-AD67-D46C1DDA604B"
+            }
+            res = requests.request(method='post', url=url, params=datas)
+            with allure.step('接口是否正常调通'):
+                if res.status_code == 200:
+                    assert True
+                else:
+                    assert False, '接口状态码非200'
+            ErrorCode = res.json()["ErrorCode"]
+            ErrorMessage = res.json()["ErrorMessage"]
+            with allure.step('QD转换撤单是否成功'):
+                if ErrorCode == 0:
+                    assert True, '撤单受理成功'
                 else:
                     assert False, ErrorMessage
--- test_组合内基金超级转换.py
+++ test_组合内基金超级转换.py
@@ -150,8 +150,134 @@ class Test_CJZH_Sub():
             with allure.step('组合内基金转换是否成功'):
                 if ErrorCode == 0:
                     assert True, '申请受理成功'
+                    clear_yaml3()
+                    BusinSerialNo = res.json()["Data"]["JumpParams"]["BusinSerialNo"]
+                    BusinessType = res.json()["Data"]["JumpParams"]["BusinessType"]
+                    write_yaml3({"BusinSerialNo": BusinSerialNo})
+                    write_yaml3({"BusinessType": BusinessType})
                 else:
                     assert False, ErrorMessage
+
+    @allure.story('815撤单 /Trade/FundTrade/RevokeOrder')
+    # 815撤单
+    def test_Trade_FundTrade_RevokeOrder(self):
+        if read_yaml3() is None:
+            pytest.skip(), '没有HH基金份额或者份额不足无法发起超级转换'
+        else:
+            url = urljoin(read_yaml1()[read_yaml4()["Env"]], "/Trade/FundTrade/RevokeOrder")
+            datas = {
+                "Password": read_yaml1()[read_yaml4()["Pas"]],
+                "UserId": read_yaml2()["CustomerNo"],
+                "BusinId": read_yaml3()["BusinSerialNo"],
+                "BusinType": read_yaml3()["BusinessType"],
+                "IsRevokedToCashBag": "false",
+                "DisplayBusinType": read_yaml3()["BusinessType"],
+
+                "PhoneType": "IPhone",
+                "ServerVersion": "6.5.8",
+                "CToken": read_yaml2()["CToken"],
+                "UToken": read_yaml2()["UToken"],
+                "MobileKey": "01F12605-0E93-4BCB-AD67-D46C1DDA604B"
+            }
+            res = requests.request(method='post', url=url, params=datas)
+            with allure.step('接口是否正常调通'):
+                if res.status_code == 200:
+                    assert True
+                else:
+                    assert False, '接口状态码非200'
+            ErrorCode = res.json()["ErrorCode"]
+            ErrorMessage = res.json()["ErrorMessage"]
+            with allure.step('超级转换撤单是否成功'):
+                if ErrorCode == 0:
+                    assert True, '撤单受理成功'
+                else:
+                    assert False, ErrorMessage
+
+
+@allure.feature('组合内基金超级转换 815 免密')
+class Test_CJZH_Sub_NP():
+    @allure.story('子账户持仓 /User/Asset/GetFundAssetListOfSubV2')
+    # 获取子账户持仓 HH型
+    def test_User_Asset_GetFundAssetListOfSubV2(self):
+        clear_yaml3()
+        url = urljoin(read_yaml1()[read_yaml4()["Env"]], "/User/Asset/GetFundAssetListOfSubV2")
+        datas = {
+            "UserId": read_yaml2()["CustomerNo"],
+            "SubAccountNo": read_yaml2()["SubAccountNo"],
+            "UnifiedType": "HH",  # 筛选类型： HH 混合； ZS 指数； GP 股票； ZQ 债券； QD QD基金； HB 货币基金
+            "BankCardNo": "",
+
+            "PhoneType": "IPhone",
+            "ServerVersion": "6.5.8",
+            "CToken": read_yaml2()["CToken"],
+            "UToken": read_yaml2()["UToken"],
+            "MobileKey": "01F12605-0E93-4BCB-AD67-D46C1DDA604B"
+        }
+        res = requests.request(method='post', url=url, params=datas)
+        with allure.step('接口是否正常调通'):
+            if res.status_code == 200:
+                assert True
+            else:
+                assert False, '接口状态码非200'
+        Res = res.json()["Data"]["AssetDetails"]
+        with allure.step('组合里是否有HH基金'):
+            if Res == []:
+                assert False, '组合内没有HH基金'
+            else:
+                assert True
+                AssetValue = res.json()["Data"]["AssetDetails"][0]["AssetValue"]
+                with allure.step('HH基金是否有足够份额'):
+                    if AssetValue > '15':
+                        assert True
+                        FundCode = res.json()["Data"]["AssetDetails"][0]["FundCode"]
+                        clear_yaml3()
+                        write_yaml3({"FundCode": FundCode})
+                    else:
+                        clear_yaml3()
+                        assert False, '份额不足无法发起'
+
+    @allure.story('持仓详情（组合） /User/home/GetShareDetail')
+    # 获取特定基金的组合份额
+    def test_User_home_GetShareDetail(self):
+        if read_yaml3() is None:
+            pytest.skip(), '没有HH基金份额或者份额不足无法发起超级转换'
+        else:
+            url = urljoin(read_yaml1()[read_yaml4()["Env"]], "/User/home/GetShareDetail")
+            datas = {
+                "UserId": read_yaml2()["CustomerNo"],
+                "SubAccountNo": read_yaml2()["SubAccountNo"],
+                "FundCode": read_yaml3()["FundCode"],
+                "IsBaseAsset": "false",
+                "TransactionAccountId": "",
+                "NeedReturnZeroVolItems": "false",
+
+                "PhoneType": "IPhone",
+                "ServerVersion": "6.5.8",
+                "CToken": read_yaml2()["CToken"],
+                "UToken": read_yaml2()["UToken"],
+                "MobileKey": "01F12605-0E93-4BCB-AD67-D46C1DDA604B"
+            }
+            res = requests.request(method='post', url=url, params=datas)
+            with allure.step('接口是否正常调通'):
+                if res.status_code == 200:
+                    assert True
+                else:
+                    assert False, '接口状态码非200'
+            with allure.step('是否存在份额可以超级转换'):  # 因为多卡才做的逻辑。这个接口没有排序也没有字段返回份额数量
+                for i in range(5):
+                    try:
+                        if res.json()["Data"]["Shares"][i - 1]["AvailableShare"] > 14:
+                            AvailableShare = res.json()["Data"]["Shares"][i - 1]["AvailableShare"]
+                            ShareId = res.json()["Data"]["Shares"][i - 1]["ShareId"]
+                            write_yaml3({"ShareId": ShareId})
+                            write_yaml3({"AvailableShare": AvailableShare})
+                            break
+                    except IndexError:
+                        pass
+
+                if read_yaml3() == {'FundCode': read_yaml3()["FundCode"]}:
+                    clear_yaml3()
+                    assert False, '没有足够的HH基金份额做815'
 
     @allure.story('交易留痕2 890免密 /Business/home/NoticeStayTrace')
     # 交易留痕2
@@ -167,8 +293,8 @@ class Test_CJZH_Sub():
                 "UToken": read_yaml2()["UToken"]
             }
             res = requests.request(method='post', url=url, params=datas)
-            TraceID2 = res.json()["Data"]["TraceID"]
-            write_yaml3({"TraceID2": TraceID2})
+            TraceID = res.json()["Data"]["TraceID"]
+            write_yaml3({"TraceID": TraceID})
 
     @allure.story('组合内基金超级转换 免密 /Business/Home/SFTransferNP')
     # 组合内基金超级转换 免密
@@ -189,7 +315,7 @@ class Test_CJZH_Sub():
                 "FundOut": read_yaml3()["FundCode"],
                 "FundAmount": read_yaml4()["FundAmount_CJZH"],
                 "LargeRedemptionFlag": "1",
-                "TraceID": read_yaml3()["TraceID2"],  # 890放一个类，TraceID会有两个
+                "TraceID": read_yaml3()["TraceID"],
 
                 "PhoneType": "IPhone",
                 "ServerVersion": "6.5.8",
@@ -208,5 +334,44 @@ class Test_CJZH_Sub():
             with allure.step('组合内基金转换是否成功'):
                 if ErrorCode == 0:
                     assert True, '申请受理成功'
+                    clear_yaml3()
+                    BusinSerialNo = res.json()["Data"]["JumpParams"]["BusinSerialNo"]
+                    BusinessType = res.json()["Data"]["JumpParams"]["BusinessType"]
+                    write_yaml3({"BusinSerialNo": BusinSerialNo})
+                    write_yaml3({"BusinessType": BusinessType})
+                else:
+                    assert False, ErrorMessage
+
+    @allure.story('815撤单 免密 /Trade/FundTrade/RevokeOrderNP')
+    # 815撤单 免密
+    def test_Trade_FundTrade_RevokeOrderNP(self):
+        if read_yaml3() is None:
+            pytest.skip(), '没有HH基金份额或者份额不足无法发起超级转换'
+        else:
+            url = urljoin(read_yaml1()[read_yaml4()["Env"]], "/Trade/FundTrade/RevokeOrderNP")
+            datas = {
+                "UserId": read_yaml2()["CustomerNo"],
+                "BusinId": read_yaml3()["BusinSerialNo"],
+                "BusinType": read_yaml3()["BusinessType"],
+                "IsRevokedToCashBag": "false",
+                "DisplayBusinType": read_yaml3()["BusinessType"],
+
+                "PhoneType": "IPhone",
+                "ServerVersion": "6.5.8",
+                "CToken": read_yaml2()["CToken"],
+                "UToken": read_yaml2()["UToken"],
+                "MobileKey": "01F12605-0E93-4BCB-AD67-D46C1DDA604B"
+            }
+            res = requests.request(method='post', url=url, params=datas)
+            with allure.step('接口是否正常调通'):
+                if res.status_code == 200:
+                    assert True
+                else:
+                    assert False, '接口状态码非200'
+            ErrorCode = res.json()["ErrorCode"]
+            ErrorMessage = res.json()["ErrorMessage"]
+            with allure.step('超级转换撤单是否成功'):
+                if ErrorCode == 0:
+                    assert True, '撤单受理成功'
                 else:
                     assert False, ErrorMessage
