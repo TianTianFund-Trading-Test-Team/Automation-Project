978c5500741ebb2223e6eb926cd7c44f87b29214
wangkangrui <649442561@qq.com>
2023-04-21 17:19:19 +0800

    version 1.1.0 断言重写 加免密解散接口

--- test_创建解散组合.py
+++ test_创建解散组合.py
@@ -25,18 +25,18 @@ class Test_Create_and_Disband_SubAccount():
             "MobileKey": "01F12605-0E93-4BCB-AD67-D46C1DDA604B"
         }
         res = requests.request(method='post', url=url, params=datas)
-        IsValid = res.json()["Data"]["IsValid"]
-        ErrorMessage = res.json()["Data"]["ErrorMessage"]
         with allure.step('接口是否正常调通'):
             if res.status_code == 200:
                 assert True
+                IsValid = res.json()["Data"]["IsValid"]
+                ErrorMessage = res.json()["Data"]["ErrorMessage"]
+                with allure.step('名称是否正常'):
+                    if IsValid == True:
+                        assert True, '名称正常'
+                    else:
+                        assert False, ErrorMessage
             else:
                 assert False, '接口状态码非200'
-        with allure.step('名称是否正常'):
-            if IsValid == True:
-                assert True, '名称正常'
-            else:
-                assert False, ErrorMessage
 
     @allure.story('验证名称是否正常 旧版 /User/SubAccount/VerifySubAccountName')
     # 验证组合名称 旧版
@@ -57,13 +57,13 @@ class Test_Create_and_Disband_SubAccount():
         with allure.step('接口是否正常调通'):
             if res.status_code == 200:
                 assert True
+                with allure.step('名称是否正常'):
+                    if IsValid == True:
+                        assert True, '名称正常'
+                    else:
+                        assert False, ErrorMessage
             else:
                 assert False, '接口状态码非200'
-        with allure.step('名称是否正常'):
-            if IsValid == True:
-                assert True, '名称正常'
-            else:
-                assert False, ErrorMessage
 
     @allure.story('选择投资风格 /User/SubAccount/SubAccountPropertyStyle')
     # 选择投资风格
@@ -107,17 +107,17 @@ class Test_Create_and_Disband_SubAccount():
         with allure.step('接口是否正常调通'):
             if res.status_code == 200:
                 assert True
+                with allure.step('组合是否创建成功'):
+                    if ErrorCode == 0:
+                        assert True, '创建组合成功'
+                        subAccountNo = res.json()["Data"]["SubAccountNo"]
+                        clear_yaml3()
+                        write_yaml3({"subAccountNo": subAccountNo})
+                    else:
+                        clear_yaml3()
+                        assert False, ErrorMessage
             else:
                 assert False, '接口状态码非200'
-        with allure.step('组合是否创建成功'):
-            if ErrorCode == 0:
-                assert True, '创建组合成功'
-                subAccountNo = res.json()["Data"]["SubAccountNo"]
-                clear_yaml3()
-                write_yaml3({"subAccountNo": subAccountNo})
-            else:
-                clear_yaml3()
-                assert False, ErrorMessage
 
     @allure.story('修改组合名称 /User/SubA/UpdateSubANP')
     # 修改组合名称
@@ -146,13 +146,13 @@ class Test_Create_and_Disband_SubAccount():
             with allure.step('接口是否正常调通'):
                 if res.status_code == 200:
                     assert True
+                    with allure.step('修改组合名称是否成功'):
+                        if ErrorCode == 0:
+                            assert True, '修改组合名称成功'
+                        else:
+                            assert False, ErrorMessage
                 else:
                     assert False, '接口状态码非200'
-            with allure.step('修改组合名称是否成功'):
-                if ErrorCode == 0:
-                    assert True, '修改组合名称成功'
-                else:
-                    assert False, ErrorMessage
 
     @allure.story('修改组合个性标签 /User/SubA/UpdateSubANP')
     # 修改组合个性标签
@@ -181,13 +181,13 @@ class Test_Create_and_Disband_SubAccount():
             with allure.step('接口是否正常调通'):
                 if res.status_code == 200:
                     assert True
+                    with allure.step('修改组合标签是否成功'):
+                        if ErrorCode == 0:
+                            assert True, '修改组合标签成功'
+                        else:
+                            assert False, ErrorMessage
                 else:
                     assert False, '接口状态码非200'
-            with allure.step('修改组合标签是否成功'):
-                if ErrorCode == 0:
-                    assert True, '修改组合标签成功'
-                else:
-                    assert False, ErrorMessage
 
     @allure.story('修改组合风格 /User/SubA/UpdateSubANP')
     # 修改组合风格
@@ -216,13 +216,13 @@ class Test_Create_and_Disband_SubAccount():
             with allure.step('接口是否正常调通'):
                 if res.status_code == 200:
                     assert True
+                    with allure.step('修改组合风格是否成功'):
+                        if ErrorCode == 0:
+                            assert True, '修改组合风格成功'
+                        else:
+                            assert False, ErrorMessage
                 else:
                     assert False, '接口状态码非200'
-            with allure.step('修改组合风格是否成功'):
-                if ErrorCode == 0:
-                    assert True, '修改组合风格成功'
-                else:
-                    assert False, ErrorMessage
 
     @allure.story('修改组合投资理念 /User/SubA/UpdateSubANP')
     # 修改组合投资理念
@@ -251,13 +251,13 @@ class Test_Create_and_Disband_SubAccount():
             with allure.step('接口是否正常调通'):
                 if res.status_code == 200:
                     assert True
+                    with allure.step('修改组合投资理念是否成功'):
+                        if ErrorCode == 0:
+                            assert True, '修改组合投资理念成功'
+                        else:
+                            assert False, ErrorMessage
                 else:
                     assert False, '接口状态码非200'
-            with allure.step('修改组合投资理念是否成功'):
-                if ErrorCode == 0:
-                    assert True, '修改组合投资理念成功'
-                else:
-                    assert False, ErrorMessage
 
     @allure.story('解散组合 /User/SubA/DisbandSubA')
     # 解散组合
@@ -278,16 +278,83 @@ class Test_Create_and_Disband_SubAccount():
                 "MobileKey": "01F12605-0E93-4BCB-AD67-D46C1DDA604B"
             }
             res = requests.request(method='post', url=url, params=datas)
-            ErrorCode = res.json()["ErrorCode"]
-            ErrorMessage = res.json()["ErrorMessage"]
             with allure.step('接口是否正常调通'):
                 if res.status_code == 200:
                     assert True
+                    ErrorCode = res.json()["ErrorCode"]
+                    ErrorMessage = res.json()["ErrorMessage"]
+                    with allure.step('组合是否解散成功'):
+                        if ErrorCode == 0:
+                            assert True, '解散组合成功'
+                            clear_yaml3()
+                        else:
+                            assert False, ErrorMessage
                 else:
                     assert False, '接口状态码非200'
-            with allure.step('组合是否解散成功'):
-                if ErrorCode == 0:
-                    assert True, '解散组合成功'
-                    clear_yaml3()
+
+    @allure.story('创建第二个组合 /User/SubA/CreateSubA')
+    # 创建第二个组合
+    def test_User_SubA_CreateSubA2(self):
+        url = urljoin(read_yaml1()[read_yaml4()["Env"]], "/User/SubA/CreateSubA")
+        datas = {
+            "UserId": read_yaml2()["CustomerNo"],
+            "CustomizeProperty": '',
+            "Property": "P1",  # 上个接口参数 组合标签
+            "Style": "S2",  # 上个接口参数 组合类型
+            "Name": read_yaml4()["Name"],
+
+            "PhoneType": "IPhone",
+            "ServerVersion": "6.5.8",
+            "CToken": read_yaml2()["CToken"],
+            "UToken": read_yaml2()["UToken"],
+            "MobileKey": "01F12605-0E93-4BCB-AD67-D46C1DDA604B"
+        }
+        res = requests.request(method='post', url=url, params=datas)
+        ErrorCode = res.json()["ErrorCode"]
+        ErrorMessage = res.json()["ErrorMessage"]
+        with allure.step('接口是否正常调通'):
+            if res.status_code == 200:
+                assert True
+                with allure.step('组合是否创建成功'):
+                    if ErrorCode == 0:
+                        assert True, '创建组合成功'
+                        subAccountNo = res.json()["Data"]["SubAccountNo"]
+                        clear_yaml3()
+                        write_yaml3({"subAccountNo": subAccountNo})
+                    else:
+                        clear_yaml3()
+                        assert False, ErrorMessage
+            else:
+                assert False, '接口状态码非200'
+
+    @allure.story('解散组合 /User/SubA/DisbandSubANP')
+    # 解散组合
+    def test_User_SubA_DisbandSubANP(self):
+        if read_yaml3() is None:
+            pytest.skip(), '创建组合失败，无法解散组合'
+        else:
+            url = urljoin(read_yaml1()[read_yaml4()["Env"]], "/User/SubA/DisbandSubANP")
+            datas = {
+                "UserId": read_yaml2()["CustomerNo"],
+                "SubAccountNo": read_yaml3()["subAccountNo"],
+
+                "PhoneType": "IPhone",
+                "ServerVersion": "6.5.8",
+                "CToken": read_yaml2()["CToken"],
+                "UToken": read_yaml2()["UToken"],
+                "MobileKey": "01F12605-0E93-4BCB-AD67-D46C1DDA604B"
+            }
+            res = requests.request(method='post', url=url, params=datas)
+            with allure.step('接口是否正常调通'):
+                if res.status_code == 200:
+                    assert True
+                    ErrorCode = res.json()["ErrorCode"]
+                    ErrorMessage = res.json()["ErrorMessage"]
+                    with allure.step('组合是否解散成功'):
+                        if ErrorCode == 0:
+                            assert True, '解散组合成功'
+                            clear_yaml3()
+                        else:
+                            assert False, ErrorMessage
                 else:
-                    assert False, ErrorMessage
+                    assert False, '接口状态码非200'
