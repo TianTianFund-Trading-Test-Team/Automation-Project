d70a9b625685da05355c55b74c97e7c97b77711d
wangkangrui <649442561@qq.com>
2023-04-24 19:24:54 +0800

    version 1.1.1 添加判断条件，第一个组合循环遍历基金

--- test_组合内基金超级转换.py
+++ test_组合内基金超级转换.py
@@ -6,11 +6,51 @@ import pytest
 import requests
 import allure
 
-from conftest import read_yaml2, write_yaml2, write_yaml3, read_yaml3, clear_yaml3, read_yaml1, read_yaml4
+from conftest import read_yaml2, write_yaml2, write_yaml3, read_yaml3, clear_yaml3, read_yaml1, read_yaml4, read_yaml5, \
+    write_yaml5, clear_yaml5
+
+# 全局变量
+g_key = 0
+
+
+def modify_g_key():
+    global g_key
+    if g_key < read_yaml5()["Count"] - 1:
+        g_key = g_key + 1
+    else:
+        assert False, '组合内基金均不支持超级转换'
 
 
 @allure.feature('组合内基金超级转换 815')
 class Test_CJZH_Sub():
+    @allure.story('持仓详情 003333 /User/home/GetShareDetail')
+    # 获取003333起购金额
+    def test_User_home_GetShareDetail_003333(self):
+        url = urljoin(read_yaml1()[read_yaml4()["Env"]], "/User/home/GetShareDetail")
+        datas = {
+            "UserId": read_yaml2()["CustomerNo"],
+            "SubAccountNo": "",
+            "FundCode": "003333",
+            "IsBaseAsset": "false",
+            "TransactionAccountId": "",
+            "NeedReturnZeroVolItems": "false",
+
+            "PhoneType": "IPhone",
+            "ServerVersion": "6.5.8",
+            "CToken": read_yaml2()["CToken"],
+            "UToken": read_yaml2()["UToken"],
+            "MobileKey": "01F12605-0E93-4BCB-AD67-D46C1DDA604B"
+        }
+        res = requests.request(method='post', url=url, params=datas)
+        with allure.step('接口是否正常调通'):
+            if res.status_code == 200:
+                assert True
+                clear_yaml5()
+                MinSg = res.json()["Data"]["MinSg"]
+                write_yaml5({"MinSg": MinSg})  # 拿003333最小申购金额
+            else:
+                assert False, '接口状态码非200'
+
     @allure.story('子账户持仓 /User/Asset/GetFundAssetListOfSubV2')
     # 获取子账户持仓 HH型
     def test_User_Asset_GetFundAssetListOfSubV2(self):
@@ -35,19 +75,16 @@ class Test_CJZH_Sub():
                 Res = res.json()["Data"]["AssetDetails"]
                 with allure.step('组合里是否有HH基金'):
                     if Res == []:
+                        clear_yaml3()
                         assert False, '组合内没有HH基金'
                     else:
                         assert True
-                        AssetValue = res.json()["Data"]["AssetDetails"][0]["AssetValue"]
-                        with allure.step('HH基金是否有足够份额'):
-                            if AssetValue > '15':
-                                assert True
-                                FundCode = res.json()["Data"]["AssetDetails"][0]["FundCode"]
-                                clear_yaml3()
-                                write_yaml3({"FundCode": FundCode})
-                            else:
-                                clear_yaml3()
-                                assert False, '份额不足无法发起'
+                        Count = res.json()["Data"]["AssetCounts"]["HH"]
+                        FundCode = res.json()["Data"]["AssetDetails"][g_key]["FundCode"]
+                        clear_yaml3()
+                        write_yaml3({"FundCode": FundCode})
+                        write_yaml5({"Count": Count})
+                        write_yaml2({"g_key": g_key})
             else:
                 assert False, '接口状态码非200'
 
@@ -55,7 +92,7 @@ class Test_CJZH_Sub():
     # 获取特定基金的组合份额
     def test_User_home_GetShareDetail(self):
         if read_yaml3() is None:
-            pytest.skip(), '没有HH基金份额或者份额不足无法发起超级转换'
+            pytest.skip(), '没有HH基金份额无法发起超级转换'
         else:
             url = urljoin(read_yaml1()[read_yaml4()["Env"]], "/User/home/GetShareDetail")
             datas = {
@@ -76,21 +113,80 @@ class Test_CJZH_Sub():
             with allure.step('接口是否正常调通'):
                 if res.status_code == 200:
                     assert True
-                    with allure.step('是否存在份额可以超级转换'):  # 因为多卡才做的逻辑。这个接口没有排序也没有字段返回份额数量
-                        for i in range(5):
+                    Enable815 = res.json()["Data"]["Enable815"]
+                    if Enable815 == True:
+                        FundNav = res.json()["Data"]["FundNav"]
+                        for i in range(5):  # 因为多卡才做的逻辑。这个接口没有排序也没有字段返回份额数量
                             try:
-                                if res.json()["Data"]["Shares"][i - 1]["AvailableShare"] > 14:
-                                    AvailableShare = res.json()["Data"]["Shares"][i - 1]["AvailableShare"]
+                                if res.json()["Data"]["Shares"][i - 1]["AvailableShare"] > (
+                                        read_yaml5()["MinSg"] / FundNav) / 0.9:
+                                    BankCardNo = res.json()["Data"]["Shares"][i - 1]["BankCardNo"]
+                                    BankAccountNo = res.json()["Data"]["Shares"][i - 1]["BankAccountNo"]
                                     ShareId = res.json()["Data"]["Shares"][i - 1]["ShareId"]
+                                    write_yaml3({"BankCardNo": BankCardNo})
+                                    write_yaml3({"BankAccountNo": BankAccountNo})
                                     write_yaml3({"ShareId": ShareId})
-                                    write_yaml3({"AvailableShare": AvailableShare})
-                                    break
+                                break
                             except IndexError:
+                                clear_yaml3()
                                 pass
-
-                        if read_yaml3() == {'FundCode': read_yaml3()["FundCode"]}:
+                        try:
+                            if read_yaml3() == {'FundCode': read_yaml3()["FundCode"]}:
+                                clear_yaml3()
+                                '该基金没有份额可以做超级转换'
+                        except TypeError:
                             clear_yaml3()
-                            assert False, '没有足够的HH基金份额做815'
+                            pass
+                    else:
+                        clear_yaml3()
+                        '该基金不支持股基转换'
+                else:
+                    assert False, '接口状态码非200'
+
+    @allure.story('转换详情（组合） /Trade/FundTrade/TransferOverview')
+    # 获取转换的最小转出
+    def test_Trade_FundTrade_TransferOverview(self):
+        if read_yaml3() is None:
+            modify_g_key()
+            self.test_User_home_GetShareDetail_003333()
+            self.test_User_Asset_GetFundAssetListOfSubV2()
+            self.test_User_home_GetShareDetail()
+            self.test_Trade_FundTrade_TransferOverview()
+        else:
+            url = urljoin(read_yaml1()[read_yaml4()["Env"]], "/Trade/FundTrade/TransferOverview")
+            datas = {
+                "UserId": read_yaml2()["CustomerNo"],
+                "FundCodeOut": read_yaml3()["FundCode"],
+                "FundCodeIn": "003333",
+                "TransferType": '',
+                "BankCardNo": read_yaml3()["BankCardNo"],
+                "BankAccountNo": read_yaml3()["BankAccountNo"],
+
+                "PhoneType": "IPhone",
+                "ServerVersion": "6.5.8",
+                "CToken": read_yaml2()["CToken"],
+                "UToken": read_yaml2()["UToken"],
+                "MobileKey": "01F12605-0E93-4BCB-AD67-D46C1DDA604B"
+            }
+            res = requests.request(method='post', url=url, params=datas)
+            with allure.step('接口是否正常调通'):
+                if res.status_code == 200:
+                    assert True
+                    FundNav = res.json()["Data"]["TranOutFund"]["FundNav"]
+                    MinRedeem = res.json()["Data"]["TranOutFund"]["MinRedeem"]
+                    MinHoldShares = res.json()["Data"]["TranOutFund"]["MinHoldShares"]
+                    AvailableVol = res.json()["Data"]["TranOutFund"]["RedeemShareAndRateList"][0]["AvailableVol"]
+                    FundAmount_CJZH = round((read_yaml5()["MinSg"] / FundNav) / 0.9, 2)+0.01  # 中台进位问题
+                    if MinRedeem < FundAmount_CJZH:
+                        if AvailableVol - FundAmount_CJZH > MinHoldShares:
+                            write_yaml3({"FundAmount_CJZH": FundAmount_CJZH})
+                        else:
+                            write_yaml3({"FundAmount_CJZH": AvailableVol})
+                    else:
+                        if AvailableVol - MinRedeem > MinHoldShares:
+                            write_yaml3({"FundAmount_CJZH": MinRedeem})
+                        else:
+                            write_yaml3({"FundAmount_CJZH": AvailableVol})
                 else:
                     assert False, '接口状态码非200'
 
@@ -129,11 +225,11 @@ class Test_CJZH_Sub():
                 "ShareID": read_yaml3()["ShareId"],
                 "FldParam": "",
 
-                "FundIn": "000001",  # 非活期宝基金，高端理财等无法买入到组合的基金
+                "FundIn": "003333",  # 非活期宝基金，高端理财等无法买入到组合的基金
                 "SubAccountNoIn": read_yaml2()["SubAccountNo"],
-                "FundCode": "000001",  # 非活期宝基金，高端理财等无法买入到组合的基金
+                "FundCode": "003333",  # 非活期宝基金，高端理财等无法买入到组合的基金
                 "FundOut": read_yaml3()["FundCode"],
-                "FundAmount": read_yaml4()["FundAmount_CJZH"],
+                "FundAmount": read_yaml3()["FundAmount_CJZH"],
                 "LargeRedemptionFlag": "1",
                 "IsAllTransfer": "false",
                 "TraceID": read_yaml3()["TraceID"],
@@ -159,6 +255,7 @@ class Test_CJZH_Sub():
                             write_yaml3({"BusinSerialNo": BusinSerialNo})
                             write_yaml3({"BusinessType": BusinessType})
                         else:
+                            clear_yaml3()
                             assert False, ErrorMessage
                 else:
                     assert False, '接口状态码非200'
@@ -167,7 +264,7 @@ class Test_CJZH_Sub():
     # 815撤单
     def test_Trade_FundTrade_RevokeOrder(self):
         if read_yaml3() is None:
-            pytest.skip(), '没有HH基金份额或者份额不足无法发起超级转换'
+            pytest.skip(), '由于没有HH基金份额，份额不足，资金池超限等原因无法发起超级转换'
         else:
             time.sleep(2)  # 单号落库时间慢，设个延迟防止报错
             url = urljoin(read_yaml1()[read_yaml4()["Env"]], "/Trade/FundTrade/RevokeOrder")
@@ -202,6 +299,35 @@ class Test_CJZH_Sub():
 
 @allure.feature('组合内基金超级转换 815 免密')
 class Test_CJZH_Sub_NP():
+
+    @allure.story('持仓详情 003333 /User/home/GetShareDetail')
+    # 获取003333起购金额
+    def test_User_home_GetShareDetail_003333(self):
+        url = urljoin(read_yaml1()[read_yaml4()["Env"]], "/User/home/GetShareDetail")
+        datas = {
+            "UserId": read_yaml2()["CustomerNo"],
+            "SubAccountNo": "",
+            "FundCode": "003333",
+            "IsBaseAsset": "false",
+            "TransactionAccountId": "",
+            "NeedReturnZeroVolItems": "false",
+
+            "PhoneType": "IPhone",
+            "ServerVersion": "6.5.8",
+            "CToken": read_yaml2()["CToken"],
+            "UToken": read_yaml2()["UToken"],
+            "MobileKey": "01F12605-0E93-4BCB-AD67-D46C1DDA604B"
+        }
+        res = requests.request(method='post', url=url, params=datas)
+        with allure.step('接口是否正常调通'):
+            if res.status_code == 200:
+                assert True
+                clear_yaml5()
+                MinSg = res.json()["Data"]["MinSg"]
+                write_yaml5({"MinSg": MinSg})  # 拿003333最小申购金额
+            else:
+                assert False, '接口状态码非200'
+
     @allure.story('子账户持仓 /User/Asset/GetFundAssetListOfSubV2')
     # 获取子账户持仓 HH型
     def test_User_Asset_GetFundAssetListOfSubV2(self):
@@ -226,19 +352,16 @@ class Test_CJZH_Sub_NP():
                 Res = res.json()["Data"]["AssetDetails"]
                 with allure.step('组合里是否有HH基金'):
                     if Res == []:
+                        clear_yaml3()
                         assert False, '组合内没有HH基金'
                     else:
                         assert True
-                        AssetValue = res.json()["Data"]["AssetDetails"][0]["AssetValue"]
-                        with allure.step('HH基金是否有足够份额'):
-                            if AssetValue > '15':
-                                assert True
-                                FundCode = res.json()["Data"]["AssetDetails"][0]["FundCode"]
-                                clear_yaml3()
-                                write_yaml3({"FundCode": FundCode})
-                            else:
-                                clear_yaml3()
-                                assert False, '份额不足无法发起'
+                        Count = res.json()["Data"]["AssetCounts"]["HH"]
+                        FundCode = res.json()["Data"]["AssetDetails"][g_key]["FundCode"]
+                        clear_yaml3()
+                        write_yaml3({"FundCode": FundCode})
+                        write_yaml5({"Count": Count})
+                        write_yaml2({"g_key": g_key})
             else:
                 assert False, '接口状态码非200'
 
@@ -246,7 +369,7 @@ class Test_CJZH_Sub_NP():
     # 获取特定基金的组合份额
     def test_User_home_GetShareDetail(self):
         if read_yaml3() is None:
-            pytest.skip(), '没有HH基金份额或者份额不足无法发起超级转换'
+            pytest.skip(), '没有HH基金份额无法发起超级转换'
         else:
             url = urljoin(read_yaml1()[read_yaml4()["Env"]], "/User/home/GetShareDetail")
             datas = {
@@ -267,27 +390,86 @@ class Test_CJZH_Sub_NP():
             with allure.step('接口是否正常调通'):
                 if res.status_code == 200:
                     assert True
-                    with allure.step('是否存在份额可以超级转换'):  # 因为多卡才做的逻辑。这个接口没有排序也没有字段返回份额数量
-                        for i in range(5):
+                    Enable815 = res.json()["Data"]["Enable815"]
+                    if Enable815 == True:
+                        FundNav = res.json()["Data"]["FundNav"]
+                        for i in range(5):  # 因为多卡才做的逻辑。这个接口没有排序也没有字段返回份额数量
                             try:
-                                if res.json()["Data"]["Shares"][i - 1]["AvailableShare"] > 14:
-                                    AvailableShare = res.json()["Data"]["Shares"][i - 1]["AvailableShare"]
+                                if res.json()["Data"]["Shares"][i - 1]["AvailableShare"] > (
+                                        read_yaml5()["MinSg"] / FundNav) / 0.9:
+                                    BankCardNo = res.json()["Data"]["Shares"][i - 1]["BankCardNo"]
+                                    BankAccountNo = res.json()["Data"]["Shares"][i - 1]["BankAccountNo"]
                                     ShareId = res.json()["Data"]["Shares"][i - 1]["ShareId"]
+                                    write_yaml3({"BankCardNo": BankCardNo})
+                                    write_yaml3({"BankAccountNo": BankAccountNo})
                                     write_yaml3({"ShareId": ShareId})
-                                    write_yaml3({"AvailableShare": AvailableShare})
-                                    break
+                                break
                             except IndexError:
+                                clear_yaml3()
                                 pass
-
-                        if read_yaml3() == {'FundCode': read_yaml3()["FundCode"]}:
+                        try:
+                            if read_yaml3() == {'FundCode': read_yaml3()["FundCode"]}:
+                                clear_yaml3()
+                                '该基金没有份额可以做超级转换'
+                        except TypeError:
                             clear_yaml3()
-                            assert False, '没有足够的HH基金份额做815'
+                            pass
+                    else:
+                        clear_yaml3()
+                        '该基金不支持股基转换'
                 else:
                     assert False, '接口状态码非200'
 
-    @allure.story('交易留痕2 890免密 /Business/home/NoticeStayTrace')
-    # 交易留痕2
-    def test_Business_home_NoticeStayTrace2(self):
+    @allure.story('转换详情（组合） /Trade/FundTrade/TransferOverview')
+    # 获取转换的最小转出
+    def test_Trade_FundTrade_TransferOverview(self):
+        if read_yaml3() is None:
+            modify_g_key()
+            self.test_User_home_GetShareDetail_003333()
+            self.test_User_Asset_GetFundAssetListOfSubV2()
+            self.test_User_home_GetShareDetail()
+            self.test_Trade_FundTrade_TransferOverview()
+        else:
+            url = urljoin(read_yaml1()[read_yaml4()["Env"]], "/Trade/FundTrade/TransferOverview")
+            datas = {
+                "UserId": read_yaml2()["CustomerNo"],
+                "FundCodeOut": read_yaml3()["FundCode"],
+                "FundCodeIn": "003333",
+                "TransferType": '',
+                "BankCardNo": read_yaml3()["BankCardNo"],
+                "BankAccountNo": read_yaml3()["BankAccountNo"],
+
+                "PhoneType": "IPhone",
+                "ServerVersion": "6.5.8",
+                "CToken": read_yaml2()["CToken"],
+                "UToken": read_yaml2()["UToken"],
+                "MobileKey": "01F12605-0E93-4BCB-AD67-D46C1DDA604B"
+            }
+            res = requests.request(method='post', url=url, params=datas)
+            with allure.step('接口是否正常调通'):
+                if res.status_code == 200:
+                    assert True
+                    FundNav = res.json()["Data"]["TranOutFund"]["FundNav"]
+                    MinRedeem = res.json()["Data"]["TranOutFund"]["MinRedeem"]
+                    MinHoldShares = res.json()["Data"]["TranOutFund"]["MinHoldShares"]
+                    AvailableVol = res.json()["Data"]["TranOutFund"]["RedeemShareAndRateList"][0]["AvailableVol"]
+                    FundAmount_CJZH = round((read_yaml5()["MinSg"] / FundNav) / 0.9, 2)+0.01
+                    if MinRedeem < FundAmount_CJZH:
+                        if AvailableVol - FundAmount_CJZH > MinHoldShares:
+                            write_yaml3({"FundAmount_CJZH": FundAmount_CJZH})
+                        else:
+                            write_yaml3({"FundAmount_CJZH": AvailableVol})
+                    else:
+                        if AvailableVol - MinRedeem > MinHoldShares:
+                            write_yaml3({"FundAmount_CJZH": MinRedeem})
+                        else:
+                            write_yaml3({"FundAmount_CJZH": AvailableVol})
+                else:
+                    assert False, '接口状态码非200'
+
+    @allure.story('交易留痕 /Business/home/NoticeStayTrace')
+    # 交易留痕
+    def test_Business_home_NoticeStayTrace(self):
         if read_yaml3() is None:
             pytest.skip(), '没有HH基金份额或者份额不足无法发起超级转换'
         else:
@@ -320,11 +502,11 @@ class Test_CJZH_Sub_NP():
                 "ShareID": read_yaml3()["ShareId"],
                 "FldParam": "",
 
-                "FundIn": "000001",  # 非活期宝基金，高端理财等无法买入到组合的基金
+                "FundIn": "003333",  # 非活期宝基金，高端理财等无法买入到组合的基金
                 "SubAccountNoIn": read_yaml2()["SubAccountNo"],
-                "FundCode": "000001",  # 非活期宝基金，高端理财等无法买入到组合的基金
+                "FundCode": "003333",  # 非活期宝基金，高端理财等无法买入到组合的基金
                 "FundOut": read_yaml3()["FundCode"],
-                "FundAmount": read_yaml4()["FundAmount_CJZH"],
+                "FundAmount": read_yaml3()["FundAmount_CJZH"],
                 "LargeRedemptionFlag": "1",
                 "TraceID": read_yaml3()["TraceID"],
 
@@ -357,7 +539,7 @@ class Test_CJZH_Sub_NP():
     # 815撤单 免密
     def test_Trade_FundTrade_RevokeOrderNP(self):
         if read_yaml3() is None:
-            pytest.skip(), '没有HH基金份额或者份额不足无法发起超级转换'
+            pytest.skip(), '由于没有HH基金份额，份额不足，资金池超限等原因无法发起超级转换'
         else:
             time.sleep(2)  # 单号落库时间慢，设个延迟防止报错
             url = urljoin(read_yaml1()[read_yaml4()["Env"]], "/Trade/FundTrade/RevokeOrderNP")
@@ -387,4 +569,3 @@ class Test_CJZH_Sub_NP():
                             assert False, ErrorMessage
                 else:
                     assert False, '接口状态码非200'
-
